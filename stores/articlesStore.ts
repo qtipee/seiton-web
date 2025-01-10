10b7890c680ebe7a1e60f2a95ca7f24f9dfebf1e
Vallat Quentin <VallatQ@ne.ch>
2025-01-10 12:16:12 +0100

    Add comments

--- stores/articlesStore.ts
+++ stores/articlesStore.ts
@@ -14,14 +14,21 @@ export const useArticlesStore = defineStore('articles', () => {
 
     /**
      * Fetch the articles from the database and store them.
+     * Call this method when the app initialises or to
+     * reset the articles in the local cache.
      */
     const initArticles = async () => {
-        console.log('--- INIT ARTICLES ---');
         const fetchedArticles = await getArticles();
         articles.value = fetchedArticles;
-        console.log(articles.value.length);
     }
 
+    /**
+     * Return the article associated to the given article ID if
+     * it exists. It first tries to get it from the local cache,
+     * otherwise it will fetch it from the database.
+     * @param articleId {string}
+     * @returns {Promise<Article | null>}
+     */
     const getArticleById = async (articleId: string): Promise<Article | null> => {
         if (!articleId) {
             return null;
