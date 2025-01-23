6010c8efb939695df55d8157a1c4411f34c3e67f
Vallat Quentin <VallatQ@ne.ch>
2025-01-23 15:19:20 +0100

    Install PWA Module + Update Loading Page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,12 +19,15 @@ export default defineNuxtConfig({
     },
     compatibilityDate: '2024-11-01',
     devtools: { enabled: true },
-    modules: ['@pinia/nuxt'],
+    modules: ['@pinia/nuxt', '@vite-pwa/nuxt'],
     css: ['~/assets/css/main.css'],
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
+    },
+    pwa: {
+        registerType: 'autoUpdate',
     },
 });
