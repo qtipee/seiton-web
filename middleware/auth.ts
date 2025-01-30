999b00bebb94b65fcfddb9703aa3f1f017e36889
qtipee <quent.vallat@gmail.com>
2025-01-30 12:05:10 +0100

    Bug Fix Login App Refresh

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,8 +1,23 @@
-export default defineNuxtRouteMiddleware(() => {
-    const { $auth } = useNuxtApp();
-    const user = $auth.currentUser;
+import { onAuthStateChanged } from 'firebase/auth';
 
-    if (!user) {
+export default defineNuxtRouteMiddleware(async () => {
+    const { $auth, $user } = useNuxtApp();
+    
+    // Ensure Firebase has restored authentication state
+    if ($user.value === null) {
+        await new Promise((resolve) => {
+            const unsubscribe = onAuthStateChanged($auth, (currentUser) => {
+                // Store user in state
+                $user.value = currentUser;
+                // Stop listening after first update
+                unsubscribe();
+                resolve(null);
+            });
+        });
+    }
+
+    // Redirect if no authenticated user
+    if (!$user.value) {
         return navigateTo('/login');
     }
 });
